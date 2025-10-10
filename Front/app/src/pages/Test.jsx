a040c6c8699c9c14e5079c1b140f3d67142a78d9
Hyeon <dolphin@kakao.com>
2025-10-10 09:23:14 +0900

    feat: Add Dialog component
    다이얼 로그 기능 추가 및 다이얼로그 애니메이션 효과 적용
    
    - Dialog 컴포넌트 추가
    - framer-motion 라이브러리를 사용하여 다이얼로그 애니메이션 추가

--- Front/app/src/pages/Test.jsx
+++ Front/app/src/pages/Test.jsx
@@ -1,12 +1,18 @@
-import Footer from '../components/Footer';
-import { Navigation } from '../components/Navigation';
+import { useState } from 'react';
+import { Button } from '../components/Button';
+import { Dialog } from '../components/Dialog';
 
 export default function Test() {
+    const [open, setOpen] = useState(false);
+
     return (
-        <>
-            <Navigation />
-            메인 콘텐츠
-            <Footer />
-        </>
+        <div>
+            <Button label="Open Dialog" onClick={() => setOpen(true)} />
+
+            <Dialog open={open} onClose={() => setOpen(false)} title="Dialog Title">
+                <p>다이얼로그 창 테스트</p>
+                <Button label="닫기" onClick={() => setOpen(false)} />
+            </Dialog>
+        </div>
     );
 }
